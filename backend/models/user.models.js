ad8a2c6d1a0dc200cb29209045fb46d316b0a9b5
Gravit09 <garvitchoudhary09@gmail.com>
2025-01-27 00:36:37 +0530

    song and album schema added

--- backend/models/user.models.js
+++ backend/models/user.models.js
@@ -2,30 +2,37 @@ import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 import Jwt from "jsonwebtoken";
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    role: {
+      type: String,
+      required: true,
+      default: "user",
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-  },
-  role: {
-    type: String,
-    required: true,
-    default: "user",
-  },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-});
+  { timestamps: true }
+);
 
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
