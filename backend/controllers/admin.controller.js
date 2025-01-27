384170788f2fd386792119849604cc5e378a2b05
Gravit09 <garvitchoudhary09@gmail.com>
2025-01-27 12:18:26 +0530

    uplaod and delete album routes added

--- backend/controllers/admin.controller.js
+++ backend/controllers/admin.controller.js
@@ -8,6 +8,8 @@ const upload = multer.fields([
   { name: "img", maxCount: 1 },
 ]);
 
+const albumCoverUpload = multer.fields([{ name: "albumCover", maxCount: 1 }]);
+
 const uploadToCloudinary = async (filePath) => {
   try {
     const res = await cloudinary.uploader.upload(filePath, {
@@ -65,3 +67,83 @@ export const uploadSong = [
     }
   },
 ];
+
+export const deleteSong = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const song = await Song.findById(id);
+    if (!song) {
+      return res.status(401).json({
+        message: "No song with this id found",
+      });
+    }
+    if (song.albumId) {
+      await Album.findByIdAndUpdate({
+        $pull: { songs: song.id },
+      });
+    }
+
+    await Song.findByIdAndUpdate({ id });
+
+    return res.status(200).json({
+      message: "Song deleted",
+    });
+  } catch (err) {
+    console.log("Some error while deleting song", err);
+    return res.status(400).json({
+      message: "Something went wrong while deleting song",
+    });
+  }
+};
+
+export const uploadAlbum = [
+  albumCoverUpload,
+  async (req, res) => {
+    try {
+      if (!req.files || !req.files.albumCover) {
+        return res.status(400).json({
+          message: "Missing upload data",
+        });
+      }
+      const { title, artists } = req.body;
+      const albumCover = req.files.albumCover[0];
+      const albumCoverUrl = await uploadToCloudinary(albumCover.path);
+
+      const newAlbum = new Album({
+        title,
+        artists,
+        albumCoverUrl,
+      });
+
+      await newAlbum.save();
+      return res.status(201).json({
+        message: "Album upload success",
+      });
+    } catch (err) {
+      console.log("upload album err", err);
+      return res.status(500).json({
+        message: "Something went wrong while uploading album",
+      });
+    }
+  },
+];
+
+export const deleteAlbum = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Song.findByIdAndDelete({
+      albumId: id,
+    });
+    await Album.findByIdAndDelete({
+      id,
+    });
+    return res.status(200).json({
+      message: "Album deleted succesfully",
+    });
+  } catch (err) {
+    console.log("err while deleting album", err);
+    return res.status(500).json({
+      message: "something went wrong while deleting album",
+    });
+  }
+};
--- backend/routes/admin.routes.js
+++ backend/routes/admin.routes.js
@@ -2,7 +2,15 @@ import { Router } from "express";
 import { uploadSong } from "../controllers/admin.controller.js";
 import { adminMiddleWare } from "../controllers/auth.contoller.js";
 import { authenticateJWT } from "../middlewares/isAuthenticated.js";
+import { deleteSong } from "../controllers/admin.controller.js";
+import { uploadAlbum } from "../controllers/admin.controller.js";
+import { deleteAlbum } from "../controllers/admin.controller.js";
 const router = Router();
 router.route("/upload").post(authenticateJWT, adminMiddleWare, uploadSong);
+router.route("/delete/:id").post(authenticateJWT, adminMiddleWare, deleteSong);
+router
+  .route("/uploadAlbum")
+  .post(authenticateJWT, adminMiddleWare, uploadAlbum);
+router.route(authenticateJWT, adminMiddleWare, deleteAlbum);
 
 export default router;
